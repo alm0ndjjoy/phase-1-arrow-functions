daf105a2a20b391eeb1e8648bbf0bb6ff41d1707
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-10-13 12:16:17 -0400

    commit tree dc6ee8ac0fdc52ea8c0ec15fff1446ed77d77785

---
+++ index.js
@@ -0,0 +1,14 @@
+// const divide = (num1, num2) => num1/num2;
+// divide(2000,100)
+
+function divide(a){
+    a = 2000;
+    return a / 100;
+}
+
+const square = x => x * x
+square(2)
+
+
+const add = (a,b) => a + b;
+add (3,4)
